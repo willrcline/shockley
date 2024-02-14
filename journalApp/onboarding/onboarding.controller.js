f79451728da0d3c2b0b406e41c3f95e21fd67987
Will Cline <willrcline.atx@gmail.com>
2024-02-13 19:48:35 -0600

    removed all imports

--- journalApp/onboarding/onboarding.controller.js
+++ journalApp/onboarding/onboarding.controller.js
@@ -1,35 +1,33 @@
-import { checkEnd } from '../helper/checkEnd.js'
-import { checkEnd } from '../helper/checkEnd';
-import { updateChatHistory, getChatHistory } from '../helper/cache';
-import { initialChatHistory } from '../helper/onboardingPromptFactory'
-import { chatCompletions } from '../../chatCompletions/chatCompletions.controller';
-import { textToSpeech } from '../../textToSpeech/textToSpeech.controller'
+const { checkEnd } = require('../helper/checkEnd.js');
+const { updateChatHistory, getChatHistory } = require('../helper/cache');
+const { initialChatHistory } = require('../helper/onboardingPromptFactory');
+const { chatCompletions } = require('../../chatCompletions/chatCompletions.controller');
+const { textToSpeech } = require('../../textToSpeech/textToSpeech.controller');
 
-const onboarding = async ({userId, audioFile}) => {
-    var responseObj = {ended: false, audioBase64: null, chatHistory: null};
+const onboarding = async ({ userId, audioFile }) => {
+    var responseObj = { ended: false, audioBase64: null, chatHistory: null };
 
     var rawTranscript = await whisper(audioFile);
-    var correctedTranscript = await correctedTranscription(rawTranscript)
+    var correctedTranscript = await correctedTranscription(rawTranscript);
 
     var inputObj = { "role": "user", "content": correctedTranscript };
     var chatHistory = getChatHistory(userId);
     if (chatHistory.length === 0) {
         chatHistory = initialChatHistory;
     }
-    var updatedChatHistory = [...chatHistory, inputObj];  
+    var updatedChatHistory = [...chatHistory, inputObj];
 
     var chatCompletion = await chatCompletions({ messages: updatedChatHistory });
 
-    if(checkEnd(chatCompletion)) {
+    if (checkEnd(chatCompletion)) {
         responseObj.ended = true;
         responseObj.chatHistory = updatedChatHistory;
-    }
-    else {
-        updateChatHistory(userId, [...updatedChatHistory, { "role": "assistant", "content": chatCompletion }])
-        responseObj.audioBase64 = await textToSpeech({text: responseText});
+    } else {
+        updateChatHistory(userId, [...updatedChatHistory, { "role": "assistant", "content": chatCompletion }]);
+        responseObj.audioBase64 = await textToSpeech({ text: responseText });
     }
 
     return responseObj;
-}
+};
 
-module.exports { onboarding }
+module.exports = { onboarding };
