630841da6e96ce33fed11d56ddec4556d6454495
Will Cline <willrcline.atx@gmail.com>
2024-02-13 19:17:56 -0600

    untested but looks good to go

--- journalApp/onboarding/onboarding.controller.js
+++ journalApp/onboarding/onboarding.controller.js
@@ -1,2 +1,34 @@
-import { systemPrompt, greetingPrompt } from '../helper/onboardingPromptFactory.js'
 import { checkEnd } from '../helper/checkEnd.js'
+import { checkEnd } from '../helper/checkEnd';
+import { updateChatHistory, getChatHistory } from '../helper/cache';
+import { initialChatHistory } from '../helper/onboardingPromptFactory'
+import { chatCompletions } from '../../chatCompletions/chatCompletions.controller';
+import { textToSpeech } from '../../textToSpeech/textToSpeech.controller'
+
+const onboarding = async ({userId, audioFile}) => {
+    var responseObj = {ended: false, audioBase64: null};
+
+    var rawTranscript = await whisper(audioFile);
+    var correctedTranscript = await correctedTranscription(rawTranscript)
+
+    var inputObj = { "role": "user", "content": correctedTranscript };
+    var chatHistory = getChatHistory(userId);
+    if (chatHistory.length === 0) {
+        chatHistory = initialChatHistory;
+    }
+    var updatedChatHistory = [...chatHistory, inputObj];  
+
+    var chatCompletion = await chatCompletions({ messages: updatedChatHistory });
+
+    if(checkEnd(chatCompletion)) {
+        responseObj.ended = true;
+    }
+    else {
+        updateChatHistory(userId, [...updatedChatHistory, { "role": "assistant", "content": chatCompletion }])
+        responseObj.audioBase64 = await textToSpeech({text: responseText});
+    }
+
+    return responseObj;
+}
+
+export { onboarding }
