3a8d13727c378796e038da5e43734e36346c29ed
Will Cline <willrcline.atx@gmail.com>
2024-02-15 16:18:22 -0600

    updated imports

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const transcription = require("./transcription/transcription.route");
-const onboarding = require("./journalApp/onboarding/onboarding.route")
 const voiceToAudio = require("./voiceToAudio/voiceToAudio.route");
 const port = process.env.PORT || 3001;
 const cors = require('cors');
@@ -11,8 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/v1/transcription', transcription)
-
-app.use('/v1/onboarding', onboarding)
 
 app.use("/v1/voiceToAudio", voiceToAudio)
 
