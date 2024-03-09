ac8432c5a9d3db9c11e560a5ae1f228ad83fce5c
Will Cline <willrcline.atx@gmail.com>
2024-03-08 20:13:22 -0600

    modified

--- chatCompletion/chatCompletion.route.js
+++ chatCompletion/chatCompletion.route.js
@@ -5,14 +5,15 @@ const { chatCompletion } = require('./chatCompletion.controller')
 router.post('/', async (req, res) => {
     console.log("chatCompletion.route req.body___", req.body)
     const {chatHistory} = req.body.data;
+    console.log("chatCompletion.route chatHistory___", chatHistory)
 
     try { 
-        var chatCompletion = await chatCompletion({messages: chatHistory});
-        console.log("voiceToChatCompletion.route responseObj___", responseObj)
-        res.status(200).send(chatCompletion);
+        var completion = await chatCompletion({messages: chatHistory});
+        console.log("chatCompletion.route completion___", completion)
+        res.status(200).send(completion);
 
     } catch (error) {
-        console.error("voiceToChatCompletion.route error___", error);
+        console.error("chatCompletion.route error___", error);
         res.status(500).send({error: 'Unexpected error processing request.'});
     }
 })
