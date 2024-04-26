3ed83a5675b8b25b55dd7e047d26de42b6f566cf
Will Cline <willrcline.atx@gmail.com>
2024-04-26 18:08:30 -0500

    changed connection to servicekey

--- firebase/connection.js
+++ firebase/connection.js
@@ -2,7 +2,7 @@ var admin = require("firebase-admin");
 var { getFirestore } = require("firebase-admin/firestore");
 
 
-var serviceAccount = require("./serviceKeys/journal-app-951a8-firebase-adminsdk-ekqtx-5a6dc81d4d.json");
+var serviceAccount = require("./serviceKeys/journal-app.json");
 
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
